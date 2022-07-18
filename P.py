a61eb9932fc198144723076798a3613fca992d0a
VLOL9 <109553608+VLOL9@users.noreply.github.com>
2022-07-18 14:16:05 -0700

    Update P.py

--- P.py
+++ P.py
@@ -5,6 +5,12 @@ L1 = ""
 L2 = ""
 L3 = ""
 L4 = ""
+L5 = ""
+L6 = ""
+L7 = ""
+L8 = ""
+L9 = ""
+L0 = "" #This is line 10
 #Strings
 S1 = ""
 S2 = ""
@@ -103,4 +109,124 @@ elif L4 == "print.8.":
 elif L4 == "print.9.":
     print(S9)
 elif L4 == "print.10.":
+    print(S0)
+if L5 == "print.1.":
+    print(S1)
+elif L5 == "print.2.":
+    print(S2)
+elif L5 == "print.3.":
+    print(S3)
+elif L5 == "print.4.":
+    print(S4)
+elif L5 == "print.5.":
+    print(S5)
+elif L5 == "print.6.":
+    print(S6)
+elif L5 == "print.7.":
+    print(S7)
+elif L5 == "print.8.":
+    print(S8)
+elif L5 == "print.9.":
+    print(S9)
+elif L5 == "print.10.":
+    print(S0)
+if L6 == "print.1.":
+    print(S1)
+elif L6 == "print.2.":
+    print(S2)
+elif L6 == "print.3.":
+    print(S3)
+elif L6 == "print.4.":
+    print(S4)
+elif L6 == "print.5.":
+    print(S5)
+elif L6 == "print.6.":
+    print(S6)
+elif L6 == "print.7.":
+    print(S7)
+elif L6 == "print.8.":
+    print(S8)
+elif L6 == "print.9.":
+    print(S9)
+elif L6 == "print.10.":
+    print(S0)
+if L7 == "print.1.":
+    print(S1)
+elif L7 == "print.2.":
+    print(S2)
+elif L7 == "print.3.":
+    print(S3)
+elif L7 == "print.4.":
+    print(S4)
+elif L7 == "print.5.":
+    print(S5)
+elif L7 == "print.6.":
+    print(S6)
+elif L7 == "print.7.":
+    print(S7)
+elif L7 == "print.8.":
+    print(S8)
+elif L7 == "print.9.":
+    print(S9)
+elif L7 == "print.10.":
+    print(S0)
+if L8 == "print.1.":
+    print(S1)
+elif L8 == "print.2.":
+    print(S2)
+elif L8 == "print.3.":
+    print(S3)
+elif L8 == "print.4.":
+    print(S4)
+elif L8 == "print.5.":
+    print(S5)
+elif L8 == "print.6.":
+    print(S6)
+elif L8 == "print.7.":
+    print(S7)
+elif L8 == "print.8.":
+    print(S8)
+elif L8 == "print.9.":
+    print(S9)
+elif L8 == "print.10.":
+    print(S0)
+if L9 == "print.1.":
+    print(S1)
+elif L9 == "print.2.":
+    print(S2)
+elif L9 == "print.3.":
+    print(S3)
+elif L9 == "print.4.":
+    print(S4)
+elif L9 == "print.5.":
+    print(S5)
+elif L9 == "print.6.":
+    print(S6)
+elif L9 == "print.7.":
+    print(S7)
+elif L9 == "print.8.":
+    print(S8)
+elif L9 == "print.9.":
+    print(S9)
+elif L9 == "print.10.":
+    print(S0)
+if L0 == "print.1.":
+    print(S1)
+elif L0 == "print.2.":
+    print(S2)
+elif L0 == "print.3.":
+    print(S3)
+elif L0 == "print.4.":
+    print(S4)
+elif L0 == "print.5.":
+    print(S5)
+elif L0 == "print.6.":
+    print(S6)
+elif L0 == "print.7.":
+    print(S7)
+elif L0 == "print.8.":
+    print(S8)
+elif L0 == "print.9.":
+    print(S9)
+elif L0 == "print.10.":
     print(S0)
