932b2a26be594552c313bc044614032df5b31743
VLOL9 <109553608+VLOL9@users.noreply.github.com>
2022-07-18 13:42:51 -0700

    Create P.py

---
+++ P.py
@@ -0,0 +1,84 @@
+#Credits to TEX 479 for making the project half the size
+
+#Lines
+L1 = ""
+L2 = ""
+L3 = ""
+#Strings
+S1 = ""
+S2 = ""
+S3 = ""
+S4 = ""
+S5 = ""
+S6 = ""
+S7 = ""
+S8 = ""
+S9 = ""
+S0 = "" #This is String 10
+#help
+#Print is same as py print but no () and no "" instead there is ..
+
+#Code
+#print Command
+if L1 == "print.1.":
+    print(S1)
+elif L1 == "print.2.":
+    print(S2)
+elif L1 == "print.3.":
+    print(S3)
+elif L1 == "print.4.":
+    print(S4)
+elif L1 == "print.5.":
+    print(S5)
+elif L1 == "print.6.":
+    print(S6)
+elif L1 == "print.7.":
+    print(S7)
+elif L1 == "print.8.":
+    print(S8)
+elif L1 == "print.9.":
+    print(S9)
+elif L1 == "print.10.":
+    print(S0)
+                      
+if L2 == "print.1.":
+    print(S1)
+elif L2 == "print.2.":
+    print(S2)
+elif L2 == "print.3.":
+    print(S3)
+elif L2 == "print.4.":
+    print(S4)
+elif L2 == "print.5.":
+    print(S5)
+elif L2 == "print.6.":
+    print(S6)
+elif L2 == "print.7.":
+    print(S7)
+elif L2 == "print.8.":
+    print(S8)
+elif L2 == "print.9.":
+    print(S9)
+elif L2 == "print.10.":
+    print(S0)
+
+if L3 == "print.1.":
+    print(S1)
+elif L3 == "print.2.":
+    print(S2)
+elif L3 == "print.3.":
+    print(S3)
+elif L3 == "print.4.":
+    print(S4)
+elif L3 == "print.5.":
+    print(S5)
+elif L3 == "print.6.":
+    print(S6)
+elif L3 == "print.7.":
+    print(S7)
+elif L3 == "print.8.":
+    print(S8)
+elif L3 == "print.9.":
+    print(S9)
+elif L3 == "print.10.":
+    print(S0)
